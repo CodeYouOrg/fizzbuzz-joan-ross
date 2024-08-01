2ea2c746ac8c80e4584e8b6348473787a011e8e6
joan-ross <rossj2904@gmail.com>
2024-08-01 04:50:03 -0400

    my attempt at the fizzbuzz challenge

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,17 @@
-# add your code here
+# Write a Python program that prints the numbers from 1 to 100. 
+# If the number is dividable by 3 print Fizz, 
+# if the number is dividable by 5 print Buzz instead of the number. 
+# If the number is dividable by 3 and 5 print FizzBuzz instead of the number.
+
+def fb_challenge(s):
+    for number in range(1,101):
+        if number % 3 == 0 :
+            str.replace(number, 'Fizz')
+        elif number % 5 == 0 :
+            str.replace(number, 'Buzz')
+        elif number % 3 == 0 and number % 5 == 0:
+            str.replace(number, 'FizzBuzz')
+            print(s)
+
+            return s
 
